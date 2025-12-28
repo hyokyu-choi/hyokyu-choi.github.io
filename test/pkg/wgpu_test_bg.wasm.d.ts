112dd2cebbd6871b9480d638c37078609750b010
Hyokyu Choi <chlgyrb111@uos.ac.kr>
2025-12-28 23:13:55 +0900

    upload wgpu test js files

---
+++ test/pkg/wgpu_test_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __wasm_bindgen_func_elem_7046: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_6732: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_7043: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_7051: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_8676: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_8675: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
